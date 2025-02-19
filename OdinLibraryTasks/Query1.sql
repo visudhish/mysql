888637cfcd67c6216b4abd9c77d0c4ec6b334592
VIswani Gudla <gudlaviswani@gmail.com>
2025-02-19 23:05:31 +0800

    Query1 Comit

---
+++ OdinLibraryTasks/Query1.sql
@@ -0,0 +1,6 @@
+#1. Books Not Loaned Out in the Last 6 Months.(joined loandate)
+select books.bookid,books.Title,loandate from books 
+left join loans on
+books.bookid = loans.bookid
+where books.bookid not in (select loans.BookID from loans where LoanDate between date_sub(curdate(),interval 6 month) and  curdate() )
+order by loandate desc ;
